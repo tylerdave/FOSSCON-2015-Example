6049893d2dab42bf4d57c773e759287b98137ec3
Dave Forgac <tylerdave@tylerdave.com>
2015-08-20 00:47:38 -0400

    rename options for consistency

--- fosscon2015/hello_argparse.py
+++ fosscon2015/hello_argparse.py
@@ -5,15 +5,15 @@ from __future__ import print_function
 import argparse
 
 def hello():
-    """ Says hello """
+    """ Argparse CLI to gree the provided NAME. """
 
     parser = argparse.ArgumentParser()
     parser.add_argument('name', help="name of person to greet")
-    parser.add_argument('-r', '--repeat', type=int, default=1,
+    parser.add_argument('-c', '--count', type=int, default=1,
             help="number of times to print the greeting")
     args = parser.parse_args()
 
-    for i in xrange(args.repeat):
+    for i in xrange(args.count):
         print("Hello, {0}!".format(args.name))
 
 if __name__ == '__main__':
