72317ffc1423e3302c587e1872cc1827397079ca
Dave Forgac <tylerdave@tylerdave.com>
2015-08-20 01:16:12 -0400

    remove debugging output

--- fosscon2015/hello_click.py
+++ fosscon2015/hello_click.py
@@ -5,12 +5,9 @@ import click
 @click.command()
 @click.argument('name')
 @click.option('--count', '-c', default=1,
-        help="number of times to print the greeting")
+        help="Number of times to print the greeting")
 def hello(name, count):
     """ Click CLI to greet the provided NAME. """
-
-    click.echo("Name: {0}".format(name))
-    click.echo("Count: {0}".format(count))
 
     for i in xrange(count):
         print("Hello, {0}!".format(name))
