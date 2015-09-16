f6fef001e45607e9033745a13579762b2907bbd4
Dave Forgac <dforgac@ag.com>
2015-09-16 11:31:40 -0400

    add argparse requirement

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
+    'argparse',
     'click==5.1',
     'colorama==0.3.3',
 ]
